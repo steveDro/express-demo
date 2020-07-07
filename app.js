65334b5ac28d543b3d53c64e5e4afc7dd7cffd2c
steveDro <akotosteven@gmail.com>
2020-07-07 10:54:12 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const customers = [
+  { id: 1, name: "Sam" },
+  { id: 2, name: "Steve" },
+  { id: 3, name: "Esty" },
+  { id: 4, name: "Becky" },
+  { id: 5, name: "Jessy" },
+  { id: 6, name: "Prince" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/api/customers", (req, res) => {
+  res.send(customers);
+});
+
+app.get("/api/post/:year/:month", (req, res) => {
+  //   res.send(req.params);
+  res.send(req.query);
+});
+app.get("/api/customer/:id", (req, res) => {
+  const customer = customers.find((p) => p.id === parseInt(req.params.id));
+  if (!customer)
+    return res.status(404).send("The customer with the given ID was not found");
+  res.send(customer);
+});
+
+app.post("/api/customers", (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const customer = {
+    id: customers.length + 1,
+    name: req.body.name,
+  };
+
+  customers.push(customer);
+  res.send(customer);
+});
+
+app.put("/api/customers/:id", (req, res) => {
+  // Look up the customer
+  const customer = customers.find((p) => p.id === parseInt(req.params.id));
+  if (!customer)
+    return res.status(404).send("The customer with the given ID was not found");
+
+  // validate the customer
+  const { error } = validateCustomer(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  //update customer
+  customer.name = req.body.name;
+  res.send(customer);
+});
+
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(customer, schema);
+}
+
+function getCustomer(id) {
+  const customer = customers.find((c) => c.id === id);
+  if (!customer) return null;
+
+  return customer;
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
